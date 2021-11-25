040a3c912865ae52e6f16709a4902be094b79135
Kehan Fu <kehanfu@gmail.com>
2021-11-25 02:44:24 +0000

    included eslint syntx changes

--- countOnly.js
+++ countOnly.js
@@ -19,24 +19,24 @@ const firstNames = [
 ];
 
 const countOnly = function(allItems, itemsToCount) {
-  const results = {}
+  const results = {};
 
-// inside the loop, 
+// inside the loop,
 // increment the counter for each item:
-//   set a property with that string key to:
-//     the value that was already there (or zero if nothing there) with 1 added to it.
+// set a property with that string key to:
+// the value that was already there (or zero if nothing there) with 1 added to it.
   for (const item of allItems) {
     // console.log(item);
-    if (itemsToCount[item]) { 
+    if (itemsToCount[item]) {
       if (results[item]) {
         results[item] += 1;
       } else {
         results[item] = 1;
       }
-     }
+    }
   }
   return results;
-}
+};
 
 const result1 = countOnly(firstNames, { "Jason": true, "Karima": true, "Fang": true, "Agouhanna": false });
 

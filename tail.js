2be8e7c6fb4714b61728e072055d79c61978772f
Kehan Fu <kehanfu@gmail.com>
2021-11-29 19:56:41 +0000

    refactor with module

--- tail.js
+++ tail.js
@@ -1,19 +1,6 @@
-const assertEqual = function(actual, expected) {
-  if (actual === expected) {
-    console.log(`✅✅✅ Assertion Passed: ${actual} === ${expected}`);
-  } else {
-    console.log(`🛑🛑🛑 Assertion Failed: ${actual} === ${expected}`);
-  }
-};
-
 const tail = function(arr) {
   console.log(arr.slice(1));
   return arr.slice(1);
 };
 
-// Test Case: Check the original array
-const words = ["Yo Yo", "Lighthouse", "Labs"];
-assertEqual(words.length, 3); // original array should still have 3 elements!
-
-const result = tail(["Hello", "Lighthouse", "Labs"]);
-assertEqual(result.join(' '), ["Lighthouse", "Labs"].join(' '));
+module.exports = tail;
